4a0fda7413596fb950ab0a587273be8c3cb99c21
Joe D <joedef@gmail.com>
2023-05-08 11:18:27 -0400

    incr dev (highlight, phase one)

--- Window.py
+++ Window.py
@@ -30,6 +30,8 @@ class Window:
         self.head_lines = 1 if head_line else 0
         self.body_count = 0
         self.scroll_pos = 0  # how far down into body are we?
+        self.light_pos = 0 # in highlight mode, where are we?
+        self.light_mode = False # whether in highlight mode
         self.rows, self.cols = 0, 0
         self.max_body_count, self.max_scroll = 0, 0
         self.handled_keys = set(keys) if isinstance(keys, (set, list)) else []
@@ -56,6 +58,10 @@ class Window:
         scr.timeout(Window.timeout_ms)
         scr.clear()
         return scr
+
+    def set_highlight(self, on=True):
+        """Set whether in highlight mode."""
+        self.light_mode = bool(on)
 
     @staticmethod
     def stop_curses():
@@ -127,24 +133,42 @@ class Window:
         if self.head_count < self.rows:
             # rectangle(self.scr, self.head_count, 0, self.head_count, self.cols-1)
             self.scr.hline(self.head_count, 0, curses.ACS_HLINE, self.cols)
-        if self.max_scroll > 0:
-            # rectangle(self.scr, self.body_base, 0, self.rows-1, 0)
-            self.scr.vline(self.body_base, 0, curses.ACS_VLINE, self.max_body_count)
-            self.scr.addstr(self._scroll_indicator_row(), 0, '|', curses.A_REVERSE)
-            self.scr.attron(curses.A_REVERSE)
-            # self.scr.vline(self._scroll_indicator_row(), 0, curses.ACS_VLINE, 1)
-            self.scr.attroff(curses.A_REVERSE)
-            self.scr.refresh()
+        indent = 0
+        if self.body_base < self.rows:
+            if self.light_mode:
+                self.light_pos = max(self.light_pos, 0)
+                self.light_pos = min(self.light_pos, self.body_count-1)
+                if self.scroll_pos > self.light_pos:
+                    # light position is below body bottom
+                    self.scroll_pos = self.light_pos
+                elif self.scroll_pos < self.light_pos - (self.max_scroll - 1):
+                    # light position is above body top
+                    self.scroll_pos = self.light_pos - (self.max_scroll - 1)
+                indent = 1
+            else:
+                self.scroll_pos = max(self.scroll_pos, 0)
+                self.scroll_pos = min(self.scroll_pos, self.max_scroll)
+                self.light_pos = self.scroll_pos
+                indent = 1 if self.body_count > self.max_scroll else 0
+
+        if indent > 0:
+            if self.light_mode:
+                self.scr.vline(self.body_base, 0, ' ', self.max_scroll)
+                pos = self.light_pos - self.scroll_pos + self.body_base
+                self.scr.addstr(pos, 0, '>', curses.A_REVERSE)
+            else:
+                pos = self._scroll_indicator_row()
+                self.scr.vline(self.body_base, 0, curses.ACS_VLINE, self.max_scroll)
+                self.scr.addstr(pos, 0, '|', curses.A_REVERSE)
+        self.scr.refresh()
 
         if self.rows > 0:
-            self.head.refresh(0, 0, 0, 1 if self.max_scroll > 0 else 0,
+            self.head.refresh(0, 0, 0, indent,
                       min(self.head_count, self.rows)-1, self.cols-1)
-        if self.body_base < self.rows:
-            self.scroll_pos = max(self.scroll_pos, 0)
-            self.scroll_pos = min(self.scroll_pos, self.max_scroll)
+
             self.body.refresh(self.scroll_pos, 0,
-                  self.body_base, 1 if self.max_scroll > 0 else 0,
-                  self.rows-1, self.cols-1)
+                  self.body_base, indent, self.rows-1, self.cols-1)
+
         if self.popup_cols > 0 and self.rows >= 4:
             popup_left = max(0, (self.cols - self.popup_cols)//2)
             popup_right = min(self.cols-1, popup_left+self.popup_cols)
@@ -157,7 +181,7 @@ class Window:
         """Popup"""
         def mod_key(key):
             return  7 if key == 10 else key
-        
+
         # need 3 extra cols for rectangle (so we don't draw in southeast corner)
         # and 3 extra rows (top/bottom/prompt)
         #      +---------+
@@ -169,7 +193,7 @@ class Window:
         row0, row9 = self.rows//2 - 2, self.rows//2 + 1
         col0 = (self.cols - (width+2)) // 2
         col9 = col0 + width + 2 - 1
-        
+
         self.scr.clear()
         win = curses.newwin(1, width, row0+2, col0+1) # input window
         rectangle(self.scr, row0, col0, row9, col9)
@@ -208,25 +232,30 @@ class Window:
                 return key # return for handling
 
             # Navigation Keys...
-            was_scroll_pos = self.scroll_pos
+            pos = self.light_pos if self.light_mode else self.scroll_pos
+            was_pos = pos
             if key in (ord('k'), curses.KEY_UP):
-                self.scroll_pos = max(self.scroll_pos - 1, 0)
+                pos -= 1
             elif key in (ord('j'), curses.KEY_DOWN):
-                self.scroll_pos += 1
+                pos += 1
             elif key in (ctl_b, curses.KEY_PPAGE):
-                self.scroll_pos -= self.max_body_count
+                pos -= self.max_body_count
             elif key in (ctl_u, ):
-                self.scroll_pos -= self.max_body_count//2
+                pos -= self.max_body_count//2
             elif key in (ctl_f, curses.KEY_NPAGE):
-                self.scroll_pos += self.max_body_count
+                pos += self.max_body_count
             elif key in (ctl_d, ):
-                self.scroll_pos += self.max_body_count//2
+                pos += self.max_body_count//2
             elif key in (ord('H'), curses.KEY_HOME):
-                self.scroll_pos = 0
+                pos = 0
             elif key in (ord('$'), curses.KEY_END):
-                self.scroll_pos = self.body_count
+                pos = self.body_count - 1
+            if self.light_mode:
+                self.light_pos = pos
+            else:
+                self.scroll_pos = pos
 
-            if self.scroll_pos != was_scroll_pos:
+            if pos != was_pos:
                 self.render()
             # ignore unhandled keys
         return None
@@ -235,16 +264,20 @@ if __name__ == '__main__':
     def main():
         """Test program"""
         def do_key(key):
-            nonlocal win, name
+            nonlocal win, name, highlight
             answer = None
             if key == ord('q'):
                 sys.exit(0)
+            if key == ord('h'):
+                highlight = not highlight
+                win.set_highlight(on=highlight)
             if key == ord('n'):
                 answer = win.answer(prompt='Type Name + Enter',
                     seed='' if name.startswith('[hit') else name)
             return answer
 
-        keys_we_handle = [ord('q'), ord('n')]
+        keys_we_handle = [ord('q'), ord('n'), ord('h')]
+        highlight = False
 
         win = Window(head_line=True, keys=keys_we_handle)
         name = "[hit 'n' to enter name]"
