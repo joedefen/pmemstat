925a7e21ccfb185e7acdede41f5700dfbcf49673
Joe D <joedef@gmail.com>
2023-05-08 21:08:07 -0400

    add module to kill a list of pids

---
+++ KillThem.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module to kill a set of processes and ensure they are really gone.
+"""
+# pylint: disable=broad-except
+
+import os
+import sys
+import signal
+import time
+import traceback
+
+class KillThem():
+    """Class to help kill a bunch of processes"""
+    def __init__(self, pids):
+        if isinstance(pids, (list, set, dict)):
+            self.pids = set(list(pids))
+        else:
+            self.pids = set([pids])
+
+    def do_kill(self):
+        """Return True of all gone, else False"""
+        sigs = [ signal.SIGTERM, signal.SIGTERM, signal.SIGTERM,
+                    signal.SIGKILL, signal.SIGKILL]
+        for sig in sigs:
+            for pid in list(self.pids):
+                try:
+                    os.kill(int(pid), sig)
+                except OSError:
+                    self.pids.discard(pid)
+            time.sleep(0.5)
+            for pid in list(self.pids):
+                # check if the process is still running
+                if not os.path.exists(f"/proc/{pid}"):
+                    self.pids.discard(pid)
+            if not self.pids:
+                break
+        return not bool(self.pids)
+
+if __name__ == '__main__':
+    def main():
+        """Test stuff .. pass in one or more pids to kill"""
+        killer = KillThem(sys.argv[1:])
+        print(f'All gone: {killer.do_kill()}')
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    except Exception as exce:
+        print("exception:", str(exce))
+        print(traceback.format_exc())
